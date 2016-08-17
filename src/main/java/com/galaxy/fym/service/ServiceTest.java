5edd98abd11a4cbe52b8379613bbda9e05badc58
fengyiming <fengyiming2012@sina.com>
2016-08-17 14:37:07 +0800

    修改项目配置

--- src/main/java/com/galaxy/fym/service/ServiceTest.java
+++
@@ -1,9 +0,0 @@
-package com.galaxy.fym.service;
-
-/**
- * Created by fengyiming on 2016/6/24.
- */
-public interface ServiceTest {
-
-    public void out();
-}
--- src/test/java/com/galaxy/fym/test/JobRunningTest.java
+++
@@ -1,8 +0,0 @@
-package com.galaxy.fym.test;
-
-
-/**
- * Created by fengyiming on 2016/8/4.
- */
-public class JobRunningTest {
-}
